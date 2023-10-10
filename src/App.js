62c78dd8af561c6e060fa93027787a08be2e16f6
Elekzander Waldron <elekwaldron@gmail.com>
2023-10-10 14:40:00 -0600

    added in the beginning of the Navbar and added come preliminary CSS items

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
+import Heptagram from './app/assets/img/heptagramLogo.png';
 import { Container, Navbar, NavbarBrand } from 'reactstrap';
-
 import './App.css';
 
 function App() {
   return (
     <div className='App'>
-      hello, hello Demonic Realm!
+      <Navbar dark sticky='top' expand='md'>
+        <Container>
+          <NavbarBrand href='/'>
+            <img src={Heptagram} alt='wubbys heptagram logo' />
+          </NavbarBrand>
+        </Container>
+      </Navbar>
+      Let's continue
     </div>
   );
 }
